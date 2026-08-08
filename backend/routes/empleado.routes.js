4a65d8975a71feb931261398ca7df4dc4f8981c3
Alejandro-Magno <alejitohernandez1991@gmail.com>
2026-08-08 11:42:12 -0500

    Se crea rutas del servidor, creamos un módulo por eso utilizamos express, vamos a utilizar como nuestra rest api para enviar y recibir datos en formato json

--- backend/routes/empleado.routes.js
+++ backend/routes/empleado.routes.js
@@ -1,6 +1,18 @@
+/**
+ * Vamos a crear rutas del servidor
+ * creamos un módulo por eso utilizamos express
+ * vamos a utilizar como nuestra rest api para
+ * enviar y recibir datos en formato json
+ */
 const express = require('express');
 const router = express.Router();
 
-// Aquí irán tus rutas más adelante
+/* generamos un ejemplo cuando le soliciten
+algo al servidor por el método GET **/
+router.get('/', (req, res) => {
+    res.json({
+        status: 'API REST funcionando'
+    });
+});
 
 module.exports = router;
