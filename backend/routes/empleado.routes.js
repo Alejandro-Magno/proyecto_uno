6bbc4285e8a3c8572381a6616c06271a475b63d1
Alejandro-Magno <alejitohernandez1991@gmail.com>
2026-08-08 12:41:31 -0500

    llama a cada uno de los métodos por defecto de petición HTTP como GET, POST, PUT, entre otros, asociados a los métodos realizados con el controlador.

--- backend/routes/empleado.routes.js
+++ backend/routes/empleado.routes.js
@@ -1,18 +1,14 @@
 /**
- * Vamos a crear rutas del servidor
  * creamos un módulo por eso utilizamos express
- * vamos a utilizar como nuestra rest api para
- * enviar y recibir datos en formato json
  */
 const express = require('express');
 const router = express.Router();
+const empleadoCtrl = require('../controllers/empleado.controller');
 
-/* generamos un ejemplo cuando le soliciten
-algo al servidor por el método GET **/
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API REST funcionando'
-    });
-});
+router.get('/', empleadoCtrl.getEmpleados); // Obtener empleados
+router.post('/', empleadoCtrl.createEmpleados); // Guardar
+router.get('/:id', empleadoCtrl.getUnicoEmpleado); // Obtiene un único empleado
+router.put('/:id', empleadoCtrl.editarEmpleado); // Actualizar datos
+router.delete('/:id', empleadoCtrl.eliminarEmpleado); // Eliminar
 
 module.exports = router;
